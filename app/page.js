67a9bc07092a2c3bbc3cee313c345dae357c0fa3
saumitra.vilankar <saumitravilankar77@gmail.com>
2023-07-03 10:33:15 +0530

    auto testimonials

--- app/page.js
+++ app/page.js
@@ -16,7 +16,24 @@ export default function Home() {
       <About />
       <AboutUs />
       <ClientsSlides />
-      <ServicePageTestimonials />
+      <ServicePageTestimonials title={'Hear what  our esteemed clients have to say about us.'} tagline={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'} testimonialData={[
+        {
+          id: 0, author: "John Doe", comment: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam omnis autem dicta laborum delectus porro nesciunt vel aspernatur corporis ea? Dolorum numquam culpa voluptatibus minima temporibus", img: "/assets/images/about.jpg", commenter: "Right Foundation"
+        },
+        {
+          id: 1, author: "John Doe", comment: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam omnis autem dicta laborum delectus porro nesciunt vel aspernatur corporis ea? Dolorum numquam culpa voluptatibus minima temporibus", img: "/assets/images/about.jpg", commenter: "Royal Runbhumi"
+        },
+        {
+          id: 2, author: "John Doe", comment: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam omnis autem dicta laborum delectus porro nesciunt vel aspernatur corporis ea? Dolorum numquam culpa voluptatibus minima temporibus", img: "/assets/images/about.jpg", commenter: "Right Foundation"
+        },
+        {
+          id: 3, author: "Saumitra Vilankar", comment: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam omnis autem dicta laborum delectus porro nesciunt vel aspernatur corporis ea? Dolorum numquam culpa voluptatibus minima",
+          img: "/assets/images/about.jpg", commenter: "Right Foundation"
+        },
+        {
+          id: 4, author: "John Doe", comment: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam omnis autem dicta laborum delectus porro nesciunt vel aspernatur corporis ea? Dolorum numquam culpa voluptatibus minima temporibus", img: "/assets/images/about.jpg", commenter: "Right Foundation"
+        },
+      ]} />
       <ContactForm />
       <CompanyAndVentures />
       <OurBlogs />
